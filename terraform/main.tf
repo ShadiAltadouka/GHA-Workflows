de4ffd8f25800cc507fa1c26589937b57999cbe3
ShadiAltadouka <shadialtadouka@yahoo.com>
2024-06-15 17:24:27 +0000

    terraform modules with EKS for pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,29 @@ resource "aws_iam_user_group_membership" "membership" {
     user = aws_iam_user.user.name
   
 }
+
+module "vpc" {
+    source = "./modules/vpc"
+}
+
+module "lt-sg" {
+    source = "./modules/launch-template-sg"
+    vpc-id = module.vpc.vpc-id
+
+    depends_on = [
+        module.vpc
+    ]
+}
+
+module "eks-iam" {
+    source = "./modules/eks-iam"
+    launch-template-id = module.lt-sg.launch-template-id
+    launch-template-version = module.lt-sg.launch-template-version
+    security-group-id = module.lt-sg.security-group-id
+    subnet-1-id = module.vpc.subnet-1
+    subnet-2-id = module.vpc.subnet-2
+
+    depends_on = [
+        module.lt-sg
+        ]
+}
