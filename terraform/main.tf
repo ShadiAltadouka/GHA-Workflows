3cce3769b96c439962043bc248d76f2b25a0d2e9
ShadiAltadouka <shadialtadouka@yahoo.com>
2024-06-15 16:18:44 +0000

    update 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 resource "aws_iam_user" "user" {
-    name = "workflow"
+    name = "GHA-workflow"
   
 }
 
-# resource "aws_iam_group" "group" {
-#     name = "GHA"
+resource "aws_iam_group" "group" {
+    name = "GHA"
   
-# }
+}
 
-# resource "aws_iam_user_group_membership" "membership" {
-#     groups = [aws_iam_group.name]
-#     user = "workflow"
+resource "aws_iam_user_group_membership" "membership" {
+    groups = [aws_iam_group.name]
+    user = aws_iam_user.user.name
   
-# }
+}
